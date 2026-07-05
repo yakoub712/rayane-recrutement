8afb0d3d89702ba6fb3424f3378b2998bfff2e65
kouba_dEV <yakoubgairaa@gmail.com>
2026-07-05 23:21:46 +0200

    modify img 6

--- public/script.js
+++ public/script.js
@@ -43,4 +43,9 @@ async function fetchMainProducts() {
 }
          document.addEventListener("DOMContentLoaded", fetchMainProducts);
 
-         
+         const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
